78b150343cd68ee587cfa1c405f8348e285065f7
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 19:59:44 +0200

    Completed test definitions for logout functionality.

--- university/tests/Feature/Api/V1/Controllers/Auth/AuthControllerLogoutTest.php
+++ university/tests/Feature/Api/V1/Controllers/Auth/AuthControllerLogoutTest.php
@@ -2,56 +2,35 @@
 
 namespace Tests\Feature\Api\V1\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class AuthControllerLogoutTest extends TestCase
 {
     use RefreshDatabase;
-
-    public final function test_logout_without_email_and_password_returns_unprocessable_content () : void
+    private string $test_url;
+    public function setUp() : void
     {
-
+        parent::setUp();
+        $this->test_url = 'api/v1/auth/logout';
     }
-    public final function test_logout_without_email_returns_unprocessable_content () : void
+    public final function test_logout_for_unauthenticated_user_returns_unauthenticated () : void
     {
+        $response = $this->getJson($this->test_url);
 
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $response->assertJsonPath('message', 'Unauthorized.');
     }
-    public final function test_logout_without_password_returns_unprocessable_content () : void
-    {
 
-    }
-    public final function test_logout_with_not_valid_email_returns_unprocessable_content () : void
+    public final function test_logout_for_authenticated_user_returns_ok () : void
     {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->getJson($this->test_url);
 
-    }
-    public final function test_logout_with_unknown_email_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_not_valid_password_min_length_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_not_valid_password_max_length_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_not_valid_password_letters_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_not_valid_password_numbers_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_not_valid_password_symbols_returns_unprocessable_content () : void
-    {
-
-    }
-    public final function test_logout_with_valid_email_and_password_returns_okau () : void
-    {
-
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonPath('message', 'Logout successfull.');
     }
 }
