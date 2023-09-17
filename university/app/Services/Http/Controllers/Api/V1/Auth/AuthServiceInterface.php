900d916472c70e9f3a113052bfd309f9b3509d1f
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 11:36:12 +0200

    Add responses to service interface and implementation.

--- university/app/Services/Http/Controllers/Api/V1/Auth/AuthServiceInterface.php
+++ university/app/Services/Http/Controllers/Api/V1/Auth/AuthServiceInterface.php
@@ -5,10 +5,13 @@ namespace App\Services\Http\Controllers\Api\V1\Auth;
 use App\Http\Requests\V1\Auth\LoginRequest;
 use App\Http\Requests\V1\Auth\LogoutRequest;
 use App\Http\Requests\V1\Auth\RegisterRequest;
+use App\Http\Responses\V1\Auth\LoginResponse;
+use App\Http\Responses\V1\Auth\LogoutResponse;
+use App\Http\Responses\V1\Auth\RegisterResponse;
 
 interface AuthServiceInterface
 {
-    public function login(LoginRequest $loginRequest);
-    public function logout (LogoutRequest $logoutRequest);
-    public function register(RegisterRequest $registerRequest);
+    public function login(LoginRequest $loginRequest) : LoginResponse;
+    public function logout (LogoutRequest $logoutRequest) : LogoutResponse;
+    public function register(RegisterRequest $registerRequest) : RegisterResponse;
 }
--- university/app/Services/Http/Controllers/Api/V1/Auth/Implementation/AuthService.php
+++ university/app/Services/Http/Controllers/Api/V1/Auth/Implementation/AuthService.php
@@ -5,20 +5,23 @@ namespace App\Services\Http\Controllers\Api\V1\Auth\Implementation;
 use App\Http\Requests\V1\Auth\LoginRequest;
 use App\Http\Requests\V1\Auth\LogoutRequest;
 use App\Http\Requests\V1\Auth\RegisterRequest;
+use App\Http\Responses\V1\Auth\LoginResponse;
+use App\Http\Responses\V1\Auth\LogoutResponse;
+use App\Http\Responses\V1\Auth\RegisterResponse;
 use App\Services\Http\Controllers\Api\V1\Auth\AuthServiceInterface;
 
 final class AuthService implements AuthServiceInterface
 {
-    public function login(LoginRequest $loginRequest)
+    public function login(LoginRequest $loginRequest) : LoginResponse
     {
-
+        return new LoginResponse();
     }
-    public function logout (LogoutRequest $logoutRequest)
+    public function logout (LogoutRequest $logoutRequest) : LogoutResponse
     {
-
+        return new LogoutResponse();
     }
-    public function register(RegisterRequest $registerRequest)
+    public function register(RegisterRequest $registerRequest) : RegisterResponse
     {
-
+        return new RegisterResponse();
     }
 }
