5209ce50457fb18736d4059567fe3acec4b92c71
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 20:21:16 +0200

    Updated Request for Register and Response.

--- university/app/Http/Controllers/Api/V1/AuthController.php
+++ university/app/Http/Controllers/Api/V1/AuthController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Auth\LoginRequest;
 use App\Http\Requests\V1\Auth\RegisterRequest;
-use App\Http\Responses\V1\Auth\RegisterResponse;
 use App\Http\Responses\V1\Response;
 use App\Services\Http\Controllers\Api\V1\Auth\AuthServiceInterface;
 use Illuminate\Http\Request;
--- university/app/Http/Responses/V1/Auth/RegisterResponse.php
+++ university/app/Http/Responses/V1/Auth/RegisterResponse.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Responses\V1\Auth;
 use App\Http\Responses\V1\Response;
+use App\Models\User;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
 
 final class RegisterResponse extends Response implements Responsable
 {
+    private string $message;
+    private int $statusCode;
+    private User $user;
+
+    public function __construct(string $message, int $statusCode, User $user)
+    {
+        $this->user = $user;
+        $this->message = $message;
+        $this->statusCode = $statusCode;
+    }
     final public function toResponse($registerRequest) : JsonResponse
     {
         return response()->json([
-            'message' => 'Register successful'
-        ]);
+            'message' => $this->message,
+            'user' => $this->user
+        ], $this->statusCode);
     }
 }
