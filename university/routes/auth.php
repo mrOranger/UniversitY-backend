ebba2d213b9e2ad015cbe18bfb8bea24a208f32e
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 11:21:15 +0200

    Setup routes and controller for Authentication.

--- university/routes/auth.php
+++ university/routes/auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout']);
 });
