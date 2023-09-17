a0964839c6d24d7212dfc6914ee4e8653611e253
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 12:31:00 +0200

    Resolve repository interface injection.

--- university/app/Providers/AppServiceProvider.php
+++ university/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Http\Controllers\Api\V1\Auth\AuthServiceInterface;
+use App\Services\Http\Controllers\Api\V1\Auth\Implementation\AuthService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(AuthServiceInterface::class, function () {
+            return new AuthService();
+        });
     }
 
     /**
