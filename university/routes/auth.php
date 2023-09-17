5973a4ec0a065d9ab5b276871a4452080e147091
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 19:46:59 +0200

    Update route method in GET.

--- university/routes/auth.php
+++ university/routes/auth.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('logout', [AuthController::class, 'logout']);
 });
