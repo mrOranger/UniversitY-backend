543208ddcfe35ecfe952d476546cb92bed6f94cb
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-17 19:49:17 +0200

    Removed authorization for login, and setup for logout.

--- university/routes/auth.php
+++ university/routes/auth.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
     Route::get('logout', [AuthController::class, 'logout']);
 });
