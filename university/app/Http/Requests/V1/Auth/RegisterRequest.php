8a7fed66b4e8e11bfee25a103333d2cebffba047
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-18 18:59:43 +0200

    Removed exists rule on email field.

--- university/app/Http/Requests/V1/Auth/RegisterRequest.php
+++ university/app/Http/Requests/V1/Auth/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
         return [
             'first_name' => ['bail', 'required', 'string'],
             'last_name' => ['bail', 'required', 'string'],
-            'email' => ['bail', 'email', 'required', 'exists:users,email'],
+            'email' => ['bail', 'email', 'required'],
             'birth_date' => ['bail', 'required', 'date'],
             'password' => ['bail', 'string', 'required', Password::min(8)->letters()->mixedCase()->numbers()->symbols()->uncompromised()],
             'password_confirmation' => ['bail', 'string', 'required', 'same:password'],
