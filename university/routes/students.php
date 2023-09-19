cd2d0b1511674f9de31f06f3abd9a6a05f7d21b5
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-19 18:33:59 +0200

    Created routes for Student resource.

---
+++ university/routes/students.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Api\V1\StudentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Student Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API Student routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+*/
+
+Route::resource('students', StudentController::class);
