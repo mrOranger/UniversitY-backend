2757f73c5aa75142493b9cbf3dbcb42a00a47f31
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-23 18:01:29 +0200

    Updates test configuration.

--- university/tests/Feature/Api/V1/Controllers/Degree/DegreeControllerGetAll.php
+++ university/tests/Feature/Api/V1/Controllers/Degree/DegreeControllerGetAll.php
@@ -8,46 +8,49 @@ use Tests\TestCase;
 
 class DegreeControllerGetAll extends TestCase
 {
-    public final function test_get_all_degrees_without_authentication_returns_unauthenticated () : void
+
+    use RefreshDatabase;
+
+    public function test_get_all_degrees_without_authentication_returns_unauthenticated () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_student_returns_unauthorized () : void
+    public function test_get_all_degrees_as_student_returns_unauthorized () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_professor_returns_unauthorized () : void
+    public function test_get_all_degrees_as_professor_returns_unauthorized () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_admin_returns_empty_response () : void
+    public function test_get_all_degrees_as_admin_returns_empty_response () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_admin_returns_one_course () : void
+    public function test_get_all_degrees_as_admin_returns_one_course () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_admin_returns_many_degrees () : void
+    public function test_get_all_degrees_as_admin_returns_many_degrees () : void
     {
 
     }
-    public final function test_get_all_degrees_as_employee_returns_empty_response () : void
-    {
-
-    }
-
-    public final function test_get_all_degrees_as_employee_returns_one_course () : void
+    public function test_get_all_degrees_as_employee_returns_empty_response () : void
     {
 
     }
 
-    public final function test_get_all_degrees_as_employee_returns_many_degrees () : void
+    public function test_get_all_degrees_as_employee_returns_one_course () : void
+    {
+
+    }
+
+    public function test_get_all_degrees_as_employee_returns_many_degrees () : void
     {
 
     }
