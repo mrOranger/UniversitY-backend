8377030e67976a7342d8cfd58dc30ed5b32de311
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-23 18:04:04 +0200

    Created service class for Degree.

---
+++ university/app/Services/Http/Controllers/Api/V1/Degree/DegreeServiceInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Http\Controllers\Api\V1\Degree;
+
+interface DegreeServiceInterface
+{
+    public function getAll();
+}
---
+++ university/app/Services/Http/Controllers/Api/V1/Degree/Implementation/DegreeService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\Http\Controllers\Api\V1\Degree\Implementation;
+use App\Services\Http\Controllers\Api\V1\Degree\DegreeServiceInterface;
+
+final class DegreeService implements DegreeServiceInterface
+{
+
+}
