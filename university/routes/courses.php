19376dc00f4a9defcd5b34a1c219e9e92da7a71d
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-23 17:23:02 +0200

    Created routes for Course Resource.

---
+++ university/routes/courses.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\V1\CourseController;
+use App\Http\Controllers\Api\V1\StudentController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Courses Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API Courses routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+*/
+
+Route::resource('courses', CourseController::class)->except([
+    'create', 'edit'
+]);
