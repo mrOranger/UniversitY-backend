84eefa418cc812faa9d2b4ea8cc86bd6b8cfa3f4
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-23 17:39:28 +0200

    Completed definition of middleware for roles.

--- university/app/Http/Middleware/V1/EnsureUserRole.php
+++ university/app/Http/Middleware/V1/EnsureUserRole.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware\V1;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserRole
@@ -13,8 +14,25 @@ class EnsureUserRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
+        if(!Auth::check()) {
+            return response()->json([
+                'message' => 'Unauthenticated'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        $user = Auth::user();
+
+        if($user->role !== 'admin') {
+            foreach ($roles as $role) {
+                if ($user->role === $role) {
+                    return $next($request);
+                }
+            }
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], Response::HTTP_FORBIDDEN);
+        }
         return $next($request);
     }
 }
