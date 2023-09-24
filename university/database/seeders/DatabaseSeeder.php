2e68a550dadf9f7459cee4787b56abbe2d2b2e68
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-24 16:54:34 +0200

    Updated main database seeder.

--- university/database/seeders/DatabaseSeeder.php
+++ university/database/seeders/DatabaseSeeder.php
@@ -13,8 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            DegreeSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
