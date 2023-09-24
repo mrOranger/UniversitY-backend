c49c98ecbff67982eed6c5b280f9996f43b4a49e
Edoardo Oranger <edo.oranger@gmail.com>
2023-09-24 16:22:04 +0200

    Created tests for update a Degree.

---
+++ university/tests/Feature/Api/V1/Controllers/Degree/DegreeControllerUpdateTest.php
@@ -0,0 +1,362 @@
+<?php
+
+namespace Tests\Feature\Api\V1\Controllers\Degree;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DegreeControllerUpdateTest extends TestCase
+{
+    use RefreshDatabase;
+    private string $test_url;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->test_url = 'api/v1/degrees';
+    }
+    final public function test_update_degree_without_authentication_returns_unauthenticated(): void
+    {
+        $response = $this->putJson($this->test_url, []);
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+        $response->assertJsonPath('message', 'Unauthenticated.');
+    }
+    final public function test_update_degree_as_student_returns_unauthorized(): void
+    {
+        $student = User::factory()->create(['role' => 'student']);
+
+        $response = $this
+            ->actingAs($student)
+            ->putJson($this->test_url, []);
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+        $response->assertJsonPath('message', 'Unauthorized.');
+    }
+
+    final public function test_update_degree_as_professor_returns_unauthorized(): void
+    {
+        $professor = User::factory()->create(['role' => 'professor']);
+
+        $response = $this
+            ->actingAs($professor)
+            ->putJson($this->test_url, []);
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+        $response->assertJsonPath('message', 'Unauthorized.');
+    }
+
+    final public function test_update_degree_as_admin_returns_name_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, []);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field is required.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_name_string_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field must be a string.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_name_max_length(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => Str::random(300),
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field must not be greater than 255 characters.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_name_already_exists(): void
+    {
+        Degree::factory()->create(['name' => 'Computer Science']);
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name has already been taken.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_code_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_code_string_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field must be a string.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_code_max_length(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => Str::random(300),
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field must not be greater than 255 characters.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_course_type_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_admin_returns_course_type_string_required(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+                'course_type' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field must be a string.');
+    }
+
+    final public function test_update_degree_as_admin_returns_course_type_not_valid(): void
+    {
+        $admin = User::factory()->create(['role' => 'admin']);
+
+        $response = $this
+            ->actingAs($admin)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+                'course_type' => 'diploma',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field must be bachelor, master or phd.');
+    }
+
+    final public function test_update_degree_as_employee_returns_name_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, []);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field is required.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_name_string_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field must be a string.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_name_max_length(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => Str::random(300),
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name field must not be greater than 255 characters.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_name_already_exists(): void
+    {
+        Degree::factory()->create(['name' => 'Computer Science']);
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.name.0', 'The name has already been taken.');
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_code_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field is required.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_code_string_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field must be a string.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_code_max_length(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => Str::random(300),
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.code.0', 'The code field must not be greater than 255 characters.');
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_course_type_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field is required.');
+    }
+
+    final public function test_update_degree_as_employee_returns_course_type_string_required(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+                'course_type' => 1234,
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field must be a string.');
+    }
+
+    final public function test_update_degree_as_employee_returns_course_type_not_valid(): void
+    {
+        $employee = User::factory()->create(['role' => 'employee']);
+
+        $response = $this
+            ->actingAs($employee)
+            ->putJson($this->test_url, [
+                'name' => 'Computer Science',
+                'code' => 'LM-31',
+                'course_type' => 'diploma',
+            ]);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonPath('errors.course_type.0', 'The course type field must be bachelor, master or phd.');
+    }
+}
