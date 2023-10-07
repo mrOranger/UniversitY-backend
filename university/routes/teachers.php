9c3e3d0dd6c2c5f8cd71b56008581f962d82dc9a
Edoardo Oranger <edo.oranger@gmail.com>
2023-10-07 11:46:36 +0200

    Update teacher routes name.

--- university/routes/teachers.php
+++ university/routes/teachers.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 */
 
-Route::resource('students', TeacherController::class)->except([
+Route::resource('teachers', TeacherController::class)->except([
     'create', 'edit',
 ])->middleware(['auth:sanctum', 'role:admin,employee,professor']);
