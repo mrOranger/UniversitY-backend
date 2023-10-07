ae4abd0b7048cfa8acba02c0182121147ccf002b
Edoardo Oranger <edo.oranger@gmail.com>
2023-10-07 12:49:37 +0200

    Fix wrong folder name and fix realationship.

--- university/app/Models/Teacher.php
+++ university/app/Models/Teacher.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Teacher extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+    ];
+
+    public function user () : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- university/app/Models/User.php
+++ university/app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Student::class);
     }
+
+    public function teacher () : HasOne
+    {
+        return $this->hasOne(Teacher::class);
+    }
 }
