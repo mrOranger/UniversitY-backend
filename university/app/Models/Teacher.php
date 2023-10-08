f5530a0032b56ec7bf4040051a44909aa44fdafc
Edoardo Oranger <edo.oranger@gmail.com>
2023-10-08 11:16:12 +0200

    Completed definition of Models for Course and relationship with Teacher.

--- university/app/Models/Teacher.php
+++ university/app/Models/Teacher.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Teacher extends Model
@@ -33,5 +34,10 @@ class Teacher extends Model
     public function user () : BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function courses () : HasMany
+    {
+        return $this->hasMany(Course::class);
     }
 }
