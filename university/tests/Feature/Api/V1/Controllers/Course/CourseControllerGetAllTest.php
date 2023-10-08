fe22ea62ee92ce78da81aa7a4a8497a487f7b335
Edoardo Oranger <edo.oranger@gmail.com>
2023-10-08 12:08:43 +0200

    Created tests for Course Controller.

---
+++ university/tests/Feature/Api/V1/Controllers/Course/CourseControllerGetAllTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Api\V1\Controllers\Course;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CourseControllerGetAllTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
