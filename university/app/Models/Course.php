9c8f83864213a8b59d6a81d13fd0fa80ec975066
Edoardo Oranger <edo.oranger@gmail.com>
2023-10-12 20:16:53 +0200

    Updated many to many relationships.

--- university/app/Models/Course.php
+++ university/app/Models/Course.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
@@ -17,5 +18,10 @@ class Course extends Model
     public function professor () : BelongsTo
     {
         return $this->belongsTo(Teacher::class);
+    }
+
+    public function students () : BelongsToMany
+    {
+        return $this->belongsToMany(Student::class);
     }
 }
--- university/app/Models/Student.php
+++ university/app/Models/Student.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Student extends Model
 {
@@ -43,5 +44,10 @@ class Student extends Model
     public function user () : BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function courses () : BelongsToMany
+    {
+        return $this->belongsToMany(Courses::class);
     }
 }
