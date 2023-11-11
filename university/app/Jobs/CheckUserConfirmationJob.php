66387c50d0407d23b48f0005669ce3fb0c10da06
Edoardo Oranger <edo.oranger@gmail.com>
2023-11-11 13:58:04 +0100

    Completed job to delete user if it is not confimed.

--- university/app/Jobs/CheckUserConfirmationJob.php
+++ university/app/Jobs/CheckUserConfirmationJob.php
@@ -2,23 +2,29 @@
 
 namespace App\Jobs;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Log;
 
 class CheckUserConfirmationJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private User $user;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -26,6 +32,14 @@ class CheckUserConfirmationJob implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        try {
+            $decryptedToken = Crypt::decrypt($this->user->confirmation);
+            $createdAt = Carbon::parse($decryptedToken['created_at']);
+            if ($createdAt != null) {
+                $this->user->delete();
+            }
+        } catch (DecryptException $exception) {
+            Log::info('Error in parsing the confirmation token for user ' . $this->user->id);
+        }
     }
 }
