b4bed1e3c0ab921e77ef813180495412efa45c93
Edoardo Oranger <edo.oranger@gmail.com>
2023-11-11 12:49:38 +0100

    Completed tests for confirm account.

--- university/tests/Feature/Api/V1/Controllers/Auth/AuthControllerConfirmAccountTest.php
+++ university/tests/Feature/Api/V1/Controllers/Auth/AuthControllerConfirmAccountTest.php
@@ -2,8 +2,8 @@
 
 namespace Tests\Feature\Api\V1\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -12,39 +12,62 @@ class AuthControllerConfirmAccountTest extends TestCase
     use RefreshDatabase;
 
     private string $route;
+    private string $confirmationCode;
 
-    public function test_confirm_account_with_not_existing_user_returns_unprocessable_content () : void
+    public function setUp() : void
+    {
+        parent::setUp();
+        $this->confirmationCode = fake()->uuid();
+    }
+
+    public function test_confirm_account_with_not_existing_user_returns_not_found () : void
     {
         $route = 'api/v1/auth/confirm/user/1/code/ab123';
 
         $response = $this->patchJson($route);
 
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
-        $response->assertJsonPath('message', 'Not a valid response');
-    }
-
-    public function test_confirm_account_with_invalid_confirmation_code_returns_unprocessable_content () : void
-    {
-
-    }
-
-    public function test_confirm_account_without_confirmation_code_returns_unprocessable_content () : void
-    {
-
-    }
-
-    public function test_confirm_account_with_user_already_confirmed_returns_conflict () : void
-    {
-
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+        $response->assertJsonPath('message', 'Unknown user.');
     }
 
     public function test_confirm_account_with_invalid_confirmation_code_returns_conflict () : void
     {
+        $user = User::factory()->createQuietly([
+            'confirmation' => $this->confirmationCode
+        ]);
+        $route = "api/v1/auth/confirm/user/$user->id/code/ab123";
 
+        $response = $this->patchJson($route);
+
+        $response->assertStatus(Response::HTTP_CONFLICT);
+        $response->assertJsonPath('message', 'Invalid confirmation code.');
     }
 
-    public function test_confirm_account_with_invalid_confirmation_code_returns_ok () : void
+    public function test_confirm_account_with_user_already_confirmed_returns_conflict () : void
     {
+        $user = User::factory()->createQuietly([
+            'confirmation' => null
+        ]);
+        $route = "api/v1/auth/confirm/user/$user->id/code/$this->confirmationCode";
 
+        $response = $this->patchJson($route);
+
+        $response->assertStatus(Response::HTTP_CONFLICT);
+        $response->assertJsonPath('message', 'Account already confirmed.');
+    }
+
+    public function test_confirm_account_with_valid_confirmation_code_returns_ok () : void
+    {
+        $user = User::factory()->createQuietly([
+            'confirmation' => $this->confirmationCode
+        ]);
+        $route = "api/v1/auth/confirm/user/$user->id/code/$this->confirmationCode";
+
+        $response = $this->patchJson($route);
+
+        $userConfimedAccount = User::find($user->id);
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonPath('message', 'Account confirmed successfully.');
+        $this->assertNull($userConfimedAccount->confirmation);
     }
 }
