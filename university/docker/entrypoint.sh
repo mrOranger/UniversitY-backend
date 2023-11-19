8a46737f9f75e688a2de8255df7171287b147e60
Edoardo Oranger <edo.oranger@gmail.com>
2023-11-19 18:00:06 +0100

    Changed commands to create the .env file.

--- university/docker/entrypoint.sh
+++ university/docker/entrypoint.sh
@@ -4,12 +4,7 @@ if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f ".env" ]; then
-    echo "Creating env file for env $APP_ENV"
-    cp .env.example .env
-else
-    echo "env file exists."
-fi
+cp .env.example .env
 
 php artisan migrate
 php artisan test
