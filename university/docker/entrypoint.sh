c6ec9c183e84f303d776ff68814c9ba30755e4f7
Edoardo Oranger <edo.oranger@gmail.com>
2023-11-21 22:12:14 +0100

    Complete configuration of Php with Docker.

--- university/docker/entrypoint.sh
+++ university/docker/entrypoint.sh
@@ -8,11 +8,7 @@ cp .env.example .env
 
 php artisan optimize
 php artisan migrate
-php artisan test
 php artisan db:seed
-php artisan key:generate
-php artisan optimize
-php artisan view:cache
 
 php-fpm -D
 nginx -g "daemon off;"
